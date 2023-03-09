0649e6fd19fb5aecc180babcedf868ad63a627b9
taisbelucik <taisbelucik@hotmail.com>
2023-03-09 17:53:46 -0300

    config routes e controller

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-const tais = guri;
+// const exp = require("constants");
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes");
 
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`servidor rodando em http://localhost:${port}`);
+});
